aad20da0f026874fff175f51579899b3ad74e7c1
Yury Cooliq <yurycooliq@gmail.com>
2020-09-15 00:38:02 +0300

    [update] Reformat variable

--- compile.js
+++ compile.js
@@ -1,4 +1,4 @@
-const contractName = 'Inbox'
+const CONTRACT_NAME = 'Inbox'
 
 // X-platform filesystem utils
 const path = require('path')
@@ -8,8 +8,8 @@ const fs = require('fs')
 const solc = require('solc')
 
 // Get source of contract
-const inboxPath = path.resolve(__dirname, 'contracts', `${contractName}.sol`)
+const inboxPath = path.resolve(__dirname, 'contracts', `${CONTRACT_NAME}.sol`)
 const source = fs.readFileSync(inboxPath, 'utf8')
 
 // Export compiled contract
-module.exports = solc.compile(source, 1).contracts[`:${contractName}`]
+module.exports = solc.compile(source, 1).contracts[`:${CONTRACT_NAME}`]
