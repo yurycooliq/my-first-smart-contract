0c7ee580b3a6220b0b7354f28416d46ff675259a
Yury Cooliq <yurycooliq@gmail.com>
2020-09-14 19:01:03 +0300

    Init

---
+++ contracts/Inbox.sol
@@ -0,0 +1,23 @@
+pragma solidity ^0.4.24;
+/*
+Inbox contract.
+Holds a public accessable string.
+*/
+contract Inbox {
+    // Nobody can see the message.
+    string public message = "default message";
+
+    // Inbox constructor.
+    // If initial message is empty,
+    // leave default message.
+    constructor(string initialMessage) public {
+        if (bytes(initialMessage).length != 0) {
+            message = initialMessage;
+        }
+    }
+
+    // Get messsage.
+    function getMessage() public view returns (string) {
+        return message;
+    }
+}
