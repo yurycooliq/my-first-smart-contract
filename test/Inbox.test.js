b616ca5046c17439dbe0e29da2675595fd7a49b3
Yury Cooliq <yurycooliq@gmail.com>
2020-09-15 11:46:48 +0300

    [update] Comments in code

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -4,9 +4,7 @@ const Web3 = require('web3')
 
 const web3 = new Web3(ganache.provider())
 const { interface, bytecode } = require("../compile")
-
 const INITIAL_MESSAGE = 'Test message'
-
 let accounts
 let inbox
 
@@ -28,20 +26,25 @@ beforeEach(async () => {
 
 describe('Inbox contract:', () => {
     it('Can deploy the contract', () => {
+        // Is address available
         assert.ok(inbox.options.address)
     })
 
     it('Has default message', async () => {
+        // Get message and compare with initial message
         const message = await inbox.methods.message().call()
         assert.equal(message, INITIAL_MESSAGE)
     })
 
     it('Changes the message', async () => {
         const newMessage = "New message"
+        // Set new message
         await inbox.methods.setMessage(newMessage).send({
             from: accounts[0]
         })
+        // Get new message
         const message = await inbox.methods.message().call()
+        // Compare
         assert.equal(message, newMessage)
     })
 })
